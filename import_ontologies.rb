02443edfe9a3c602128939bb887855aca18c1640
Vincent Emonet <vincent.emonet@gmail.com>
2015-06-22 17:44:25 +0200

    Can now import submission from BioPortal to BioPortal and specify groups/categories

--- import_ontologies.rb
+++ import_ontologies.rb
@@ -8,10 +8,11 @@ require_relative 'src/ontology_uploader.rb'
 #http://data.bioontology.org/ontologies/EDAM/latest_submission
 
 
-restUrl = bp_url_input
+#restUrl = bp_url_input
 #restUrl = "http://data.stageportal.lirmm.fr"
 #restUrl = "http://data.agroportal.lirmm.fr"
-apikey = bp_apikey_input
+#apikey = bp_apikey_input
+user = "admin"
 
 
 jsonImport = "import_array.json"
@@ -20,17 +21,16 @@ jsonImport = "import_array.json"
 ontologiesPath = "/srv/data/ontologies"
 
 
-# noinspection RubyArgCount
-ontologyUploader = OntologyUploader.new(bp_url_output, bp_apikey_output)
+ontologyUploader = OntologyUploader.new(bp_url_output, bp_apikey_output, user)
 
 file = File.read(jsonImport)
-
 ontologiesArray = JSON.parse(file)
 
 
 ontologiesArray.each do |onto|
   puts onto["acronym"]
-  puts ontologyUploader.get_ontology_data(onto["acronym"], onto["source"])
-  #puts ontologyUploader.create_ontology(onto)
-  #puts ontologyUploader.upload_submission(onto, ontologiesPath)
+  ontoData = ontologyUploader.get_submission_data(onto)
+    # Get an array with a hash with infos to create the ontology and another hash with info to import the submission
+  puts ontologyUploader.create_ontology(ontoData[0])
+  puts ontologyUploader.upload_submission(ontoData[1], onto["acronym"])
 end
--- src/ontology_uploader.rb
+++ src/ontology_uploader.rb
@@ -2,46 +2,67 @@ require_relative '../config.rb'
 
 class OntologyUploader
 
-  attr_reader :restUrl, :apikey, :uploadDate
+  attr_reader :restUrl, :apikey, :user, :uploadDate
 
-  def initialize(restUrl, apikey)
+  def initialize(restUrl, apikey, user)
     @apikey=apikey
     @restUrl=restUrl
+    @user = user
     @uploadDate = Time.now.strftime("%Y-%m-%d")
   end
 
-#http://data.bioontology.org/ontologies/EDAM/latest_submission
-  def get_ontology_data(acronym, source)
+  def get_submission_data(ontoInfo)
     # Get the metadata for the ontology we want to upload (depend on the source)
 
-    if source == "ncbo_bioportal"
-      # For NCBO it call the last submission
+    if ontoInfo["source"] == "ncbo_bioportal"
+      # For NCBO it call the last submission and get all data from it, except for groups and categories
 
-      getSub = "#{bp_url_input}/ontologies/#{acronym}/latest_submission?apikey=#{bp_apikey_input}"
-      ontologies_json = JSON.parse(Net::HTTP.get(URI.parse(getSub)))
+      getSub = "#{bp_url_input}/ontologies/#{ontoInfo["acronym"]}/latest_submission?apikey=#{bp_apikey_input}"
+      hash = JSON.parse(Net::HTTP.get(URI.parse(getSub)))
+
+      ontology_hash = {
+          "acronym": ontoInfo["acronym"],
+          "name": hash["ontology"]["name"],
+          "group": ontoInfo["group"],
+          "hasDomain": ontoInfo["hasDomain"],
+          "administeredBy": [@user]}
+
+      # Get the contacts for the submission
+      contacts = []
+      hash["contact"].each do |contact|
+        contacts.push({"name": contact["name"], "email": contact["email"]})
+      end
+
+      submission_hash = {
+          "contact": contacts,
+          "ontology": "#{@restUrl}/ontologies/#{ontoInfo["acronym"]}",
+          "hasOntologyLanguage": hash["hasOntologyLanguage"],
+          "released": hash["released"],
+          "description": hash["description"],
+          "status": "production",
+          "version": hash["version"],
+          "homepage": hash["homepage"],
+          "documentation": hash["documentation"],
+          "publication": hash["publication"],
+          "pullLocation": "#{bp_url_input}/ontologies/#{ontoInfo["acronym"]}/submissions/#{hash["submissionId"]}/download?apikey=#{bp_apikey_input}"
+      }
     end
-    return ontologies_json
+
+    return [ontology_hash, submission_hash]
   end
 
 
-
-  #TODO: Pas encore modifié
   def create_ontology(hash)
     # Create a new ontology
 
     uri = URI.parse(@restUrl)
     http = Net::HTTP.new(uri.host, uri.port)
 
-    req = Net::HTTP::Put.new("/ontologies/#{hash["acronym"]}")
+    req = Net::HTTP::Put.new("/ontologies/#{hash[:acronym]}")
     req['Content-Type'] = "application/json"
     req['Authorization'] = "apikey token=#{@apikey}"
 
-    if (hash["viewingRestriction"] == "private")
-      # In case of private ontology
-      req.body = { "acronym": hash["acronym"], "name": hash["name"], "group": hash["group"], "hasDomain": hash["hasDomain"], "administeredBy": [@user], "viewingRestriction": "private", "acl": hash["acl"]}.to_json
-    else
-      req.body = { "acronym": hash["acronym"], "name": hash["name"], "group": hash["group"], "hasDomain": hash["hasDomain"], "administeredBy": [@user]}.to_json
-    end
+    req.body = hash.to_json
 
     response = http.start do |http|
       http.request(req)
@@ -50,13 +71,16 @@ class OntologyUploader
     return response
   end
 
-#TODO: Pas encore modifié
-  def upload_submission(hash, filePath)
+
+  def upload_submission(hash, acronym)
+    # Upload a submission
 
     uri = URI.parse(@restUrl)
     http = Net::HTTP.new(uri.host, uri.port)
 
-    req = Net::HTTP::Post.new("/ontologies/#{hash["acronym"]}/submissions")
+    puts hash["acronym"]
+
+    req = Net::HTTP::Post.new("/ontologies/#{acronym}/submissions")
     req['Content-Type'] = "application/json"
     req['Authorization'] = "apikey token=#{@apikey}"
 
@@ -65,23 +89,9 @@ class OntologyUploader
     else
       releaseDate = @uploadDate
     end
-
-    # hasOntologyLanguage: OWL, UMLS, SKOS, OBO
+    
     # status: alpha, beta, production, retired
-    req.body = {
-        "contact": [{"name": hash["contact"],"email": hash["mail"]}],
-        "ontology": "#{@restUrl}/ontologies/#{hash["acronym"]}",
-        "hasOntologyLanguage": hash["format"],
-        "released": releaseDate,
-        "description": hash["description"],
-        "status": "production",
-        "version": hash["version"],
-        "homepage": hash["homepage"],
-        "documentation": hash["documentation"],
-        "publication": hash["publication"],
-        "naturalLanguage": "fr",
-        "uploadFilePath": filePath + "/" + hash["uploadPath"]
-    }.to_json
+    req.body = hash.to_json
 
     response = http.start do |http|
       http.request(req)
